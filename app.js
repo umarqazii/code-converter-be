537c2bf99e089a3ad45330f4b09a42ebfbe79fda
Umar Qazi <umarqazii983@gmail.com>
2024-10-09 10:43:26 +0500

    code conversion being done for multiple languages

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-// app.js
 const express = require('express');
 const dotenv = require('dotenv');
 const codeConverterRoutes = require('./routes/codeConverterRoutes');
+const cors = require('cors'); // Import cors
 
 // Load environment variables from .env file
 dotenv.config();
 
-const app = express();
+// Initialize Express app
+const app = express(); // <--- Move this above the use of middleware
+
+// Enable CORS
+app.use(cors());
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Use code conversion routes
 app.use('/', codeConverterRoutes);
+
 
 // Starting the server
 const PORT = process.env.PORT || 5000;
